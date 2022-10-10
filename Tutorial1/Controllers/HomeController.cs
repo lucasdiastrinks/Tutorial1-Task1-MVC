4372cea3c9250edd2eeb171049ac0c5515d59593
lucas.oliveira <lucas.oliveira@trinks.com>
2022-10-10 16:38:49 -0300

    Adicionar arquivos de projeto.

---
+++ Tutorial1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tutorial1.Models;
+
+namespace Tutorial1.Controllers {
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
